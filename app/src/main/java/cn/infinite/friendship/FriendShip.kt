887eb7a2c7964595bb842b83258f2c198e64e524
infinite-Zh <zhxuwork@163.com>
2019-11-19 17:34:15 +0800

    画波浪

--- app/src/main/java/cn/infinite/friendship/FriendShip.kt
+++ app/src/main/java/cn/infinite/friendship/FriendShip.kt
@@ -22,18 +22,45 @@ class FriendShip : View {
     private val mWavePaint = Paint().apply {
         color = Color.parseColor("#00a1ff")
         strokeWidth = 5f
-        style=Paint.Style.STROKE
+        style = Paint.Style.STROKE
     }
 
-    private var mPath=Path()
-    private var mOffset=0f
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        offset=-w.toFloat()
+    }
+
+    private var mPath = Path()
+    private var offset = 0f
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas?.translate(0f,500f)
-        mPath.moveTo(0f+mOffset,0f+mOffset)
+        generatePath()
+        canvas?.drawPath(mPath, mWavePaint)
 
-        mPath.quadTo(mWaveWidth/2+mOffset,mWaveHeight,mWaveWidth+mOffset,0f)
-//        mPath.rQuadTo()
-        canvas?.drawPath(mPath,mWavePaint)
+        offset += 1f
+        if (offset== 0f) {
+            offset = -measuredWidth.toFloat()
+        }
+        invalidate()
     }
+
+    private fun generatePath() {
+
+        val num = measuredWidth / (mWaveWidth.times(2)).toInt() + 1
+        mPath.reset()
+        mPath.moveTo(0f + offset, measuredHeight / 2.toFloat())
+
+        for (i in 1 until num.times(2)) {
+                mPath.quadTo(
+                    mWaveWidth.times(i) / 2 + offset, mWaveHeight + measuredHeight / 2,
+                    mWaveWidth.times(i) + offset, measuredHeight / 2.toFloat()
+                )
+
+                mPath.quadTo(
+                    3 * mWaveWidth.times(i) / 2 + offset, measuredHeight / 2 - mWaveHeight,
+                    mWaveWidth.times(i) * 2 + offset, measuredHeight / 2.toFloat()
+                )
+            }
+        }
+
 }
